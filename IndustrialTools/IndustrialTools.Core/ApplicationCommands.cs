5672add7c9150e33c1903e5ff93cfbb764a8f9c9
Lincington <lincington@163.com>
2025-04-23 21:25:53 +0800

    add menu

---
+++ IndustrialTools/IndustrialTools.Core/ApplicationCommands.cs
@@ -0,0 +1,15 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+
+namespace IndustrialTools.Core
+{
+    public interface IApplicationCommands
+    {
+        CompositeCommand Connection { get; }
+    }
+
+    public class ApplicationCommands : IApplicationCommands
+    {
+        public CompositeCommand Connection  { get; } = new CompositeCommand();
+    }
+}
