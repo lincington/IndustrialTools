4248c0a988fd27d4b0dfc5f085bce73d91b84184
lincington <lincington@163.com>
2026-01-02 17:36:26 +0800

    rr67ttytyt

--- IndustrialTools/Common/IndustrialTools.Lib/PaddleOCRSharpHelper.cs
+++ IndustrialTools/Common/IndustrialTools.Lib/PaddleOCRSharpHelper.cs
@@ -7,10 +7,8 @@ namespace IndustrialTools.Lib
     {
        
         public PaddleOCRSharpHelper()
-        {
-  
+        { 
         }
-     
 
         private PaddleOCREngine engine;
         OCRModelConfig config = null;
